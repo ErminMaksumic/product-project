cb06f9de83f3e6cbf96cf5028a00be4978a6a9cb
ErminMaksumic <maksumicermin@gmail.com>
2024-02-01 17:03:46 +0100

    removed bind

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'bindSearchObject' => \App\Http\Middleware\BindSearchObject::class,
     ];
 }
