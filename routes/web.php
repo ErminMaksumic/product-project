22821ea5efe8ffa548bfb738be0f77ce8e386e8c
ErminMaksumic <maksumicermin@gmail.com>
2024-02-14 11:43:31 +0100

    update_routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('http://localhost:8000/login');
 });
 
 Route::get('/dashboard', function () {
